5354a8c62d8aa32e7759ea315bdb3da5f7481aff
Caio Machado <caio.student@outlook.com>
2021-09-05 23:51:45 -0300

    login

--- agenda/core/models.py
+++ agenda/core/models.py
@@ -25,6 +25,9 @@ class Evento(models.Model):
     def get_data_input_evento(self):
         return self.data_evento.strftime('%Y-%m-%dT%H:%M')
 
+    def get_data_atual(self):
+        return datetime.now()
+
     def get_evento_atrasado(self):
         if self.data_evento < datetime.now():
             return True
